e590e6b558d72e5f813b3db857b9fa57b4960a59
dvdfu <davidf1212@gmail.com>
2017-04-22 14:18:55 -0700

    background rain

--- src/states/game.lua
+++ src/states/game.lua
@@ -4,7 +4,9 @@ local Timer = require 'modules.hump.timer'
 local Player = require 'src.objects.player'
 local Raindrop = require 'src.objects.raindrop'
 local Camera = require 'src.camera'
+local Constants = require 'src.constants'
 local Objects = require 'src.objects'
+local Rain = require 'src.rain'
 
 local Game = {}
 
@@ -22,9 +24,14 @@ function Game:enter()
     self.objects = Objects()
     self.player = Player(self.objects, 180, 120)
     self.camera = Camera(self.player, { damping = 12 })
+    self.rain = Rain()
+
     self.timer = Timer.new()
     self.timer:every(60, function()
         Raindrop(self.objects, 200, -100, math.random(4, 50))
+    end)
+    self.timer:every(1, function()
+        self.rain:add(math.random() * Constants.GAME_WIDTH)
     end)
 end
 
@@ -33,6 +40,7 @@ function Game:update(dt)
     self.objects:update(dt)
     self.camera:follow(self.player)
     self.camera:update(dt)
+    self.rain:update(dt)
     self.timer:update(dt)
 end
 
@@ -42,6 +50,7 @@ function Game:draw()
     for x = camX, love.graphics.getWidth(), 480 do
         love.graphics.draw(sprites.backgroundBlur, x, 0)
     end
+    self.rain:draw()
     self.camera:draw(function()
         self.player:draw()
         self.objects:draw()
