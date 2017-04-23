7cd51e34990f82af99dcf927dad4d08b738f5141
dvdfu <davidf1212@gmail.com>
2017-04-23 15:20:30 -0700

    splash animation

--- src/objects/raindrop.lua
+++ src/objects/raindrop.lua
@@ -1,4 +1,5 @@
 local Class = require 'modules.hump.class'
+local Timer = require 'modules.hump.timer'
 local Vector = require 'modules.hump.vector'
 local Object = require 'src.objects.object'
 local Animation = require 'src.animation'
@@ -10,6 +11,7 @@ local sprites = {
     small = love.graphics.newImage('res/raindrop_small.png'),
     medium = love.graphics.newImage('res/raindrop_medium.png'),
     large = love.graphics.newImage('res/raindrop_large.png'),
+    splash = love.graphics.newImage('res/raindrop_splash.png'),
 }
 
 local sounds = {
@@ -21,6 +23,9 @@ function Raindrop:init(objects, x, y, radius)
     self:build(objects:getWorld(), x, y, radius)
     self.radius = radius
     self.wobble = 0
+    self.dead = false
+    self.splash = Animation(sprites.splash, 3, 4)
+    self.timer = Timer.new()
     self:addTag('raindrop')
 end
 
@@ -36,20 +41,39 @@ function Raindrop:update(dt)
     Object.update(self, dt)
     self.body:setLinearVelocity(0, 4)
     self.wobble = (self.wobble + dt / 30) % 1
+    self.timer:update(dt)
+
+    if self.dead then
+        self.splash:update(dt)
+    end
 end
 
 function Raindrop:collide(col, other)
     if other:hasTag('flower') or other:hasTag('frog') then
-        sounds.droplet:setPitch(1.2 - self.radius / 60)
-        sounds.droplet:play()
-        self.body:destroy()
+        self:die()
     end
 end
 
+function Raindrop:die()
+    if self.dead then return end
+    self.dead = true
+    self.fixture:setSensor()
+    sounds.droplet:setPitch(1.2 - self.radius / 60)
+    sounds.droplet:play()
+    self.timer:after(10, function()
+        self.body:destroy()
+    end)
+end
+
 function Raindrop:draw()
-    local x, y = self.body:getPosition()
-    local wobble = math.sin(self.wobble * math.pi * 2) / 16
+    local x, y = self.pos:unpack()
     local radius = self.radius + 3 -- buffer spaces
+    if self.dead then
+        self.splash:draw(x, y, 0, radius / 32, radius / 32, 32, 32)
+        return
+    end
+
+    local wobble = math.sin(self.wobble * math.pi * 2) / 16
     if radius > 16 then
         local scale = radius / 32
         love.graphics.draw(sprites.large, x, y, 0, scale + wobble, scale - wobble, 32, 32)
