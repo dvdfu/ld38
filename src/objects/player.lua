a8805e9af2d31bbe95074b92656769db2f758f82
Seikun Kambashi <seikun_lee@hotmail.com>
2017-04-23 18:20:38 -0400

    prevent shooting all bees

--- src/objects/player.lua
+++ src/objects/player.lua
@@ -5,7 +5,7 @@ local Bullet = require 'src.objects.bullet'
 local Constants = require 'src.constants'
 
 local Player = Class.new()
-Player.BEE_COUNT = 100
+Player.BEE_COUNT = 5
 Player.MOVE_SPEED = 3
 
 local sprites = {
@@ -87,7 +87,7 @@ function Player:setMouse(pos)
 end
 
 function Player:shoot()
-    if self:numBees() < 1 then return end
+    if self:numBees() < 2 then return end
     local bullet_bee
     for k, bee in pairs(self.bees) do
         if bee:isDead() then
