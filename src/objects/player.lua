1d09f263ba5e072f0f42764efa718b127df81428
dvdfu <davidf1212@gmail.com>
2017-04-24 10:09:35 -0700

    prevent player going past end

--- src/objects/player.lua
+++ src/objects/player.lua
@@ -70,6 +70,9 @@ function Player:update(dt)
 
     if self.pos.y < 0 then self.pos.y = 0 end
     if self.pos.y > Constants.GAME_HEIGHT then self.pos.y = Constants.GAME_HEIGHT end
+    if self.pos.x > Constants.TOTAL_CHUNKS * Constants.GAME_WIDTH then
+        self.pos.x = Constants.TOTAL_CHUNKS * Constants.GAME_WIDTH
+    end
 end
 
 function Player:getPosition()
