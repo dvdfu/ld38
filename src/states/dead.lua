709fe955c5ee70488b019b52e9373a2122d6db44
dvdfu <davidf1212@gmail.com>
2017-04-24 15:14:57 -0700

    death state

--- src/states/dead.lua
+++ src/states/dead.lua
@@ -1,22 +1,37 @@
-local Constants = require 'src.constants'
 local Gamestate = require 'modules.hump.gamestate'
+local Timer = require 'modules.hump.timer'
 local Transition = require 'src.states.transition'
+local Constants = require 'src.constants'
 local Music = require 'src.music'
 
 local sprites = {
-    backgroundBlur = love.graphics.newImage('res/background_blur.png'),
+    background = love.graphics.newImage('res/ending_death.png'),
 }
 
 local Dead = {}
 
-function Dead:init()
-    Music.init()
+function Dead:enter()
     self.transition = Transition()
+    self.transition:fadeIn()
+    self.transitioning = false
+    self.timer = Timer.new()
+    self.state = {
+        opacity = 0,
+        textPos = Constants.GAME_HEIGHT / 2 + 50,
+        textOpacity = 0
+    }
+
+    self.timer:after(200, function()
+        self.timer:tween(60, self.state, {
+            opacity = 125,
+            textPos = Constants.GAME_HEIGHT / 2,
+            textOpacity = 255
+        }, 'in-out-cubic')
+    end)
 end
 
-function Dead:enter()
-    self.transitioning = false
-    self.transition:fadeIn()
+function Dead:update(dt)
+    self.timer:update(dt)
 end
 
 function Dead:keypressed(key)
@@ -34,11 +49,17 @@ end
 
 function Dead:update(dt)
     self.transition:update(dt)
+    self.timer:update(dt)
 end
 
 function Dead:draw()
-    love.graphics.draw(sprites.backgroundBlur, 0, 0)
-    love.graphics.draw(sprites.backgroundBlur, 480, 0)
+    love.graphics.draw(sprites.background, 0, 0)
+
+    love.graphics.setColor(0, 0, 0, self.state.opacity)
+    love.graphics.rectangle('fill', 0, 0, Constants.GAME_WIDTH, Constants.GAME_HEIGHT)
+    love.graphics.setColor(255, 255, 255, self.state.textOpacity)
+    love.graphics.printf("Try again?", Constants.GAME_WIDTH / 2 - 100, self.state.textPos, 200, 'center')
+    love.graphics.setColor(255, 255, 255)
 
     self.transition:draw()
 end
