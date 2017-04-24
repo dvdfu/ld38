674c74b14f5373ce43e4ff8d3480fbb842ce2bb5
Hamdan Javeed <hamdan@hamdanjaveed.com>
2017-04-24 02:55:27 -0700

    Setup gh pages

--- conf.lua
+++
@@ -1,7 +0,0 @@
-function love.conf(t)
-    t.window.title = 'LD38'
-    t.window.resizable = false
-    t.window.vsync = true
-    t.window.width = 1200
-    t.window.height = 675
-end
--- src/constants.lua
+++
@@ -1,8 +0,0 @@
-return {
-    GAME_WIDTH = 1200 / 2,
-    GAME_HEIGHT = 675 / 2,
-    DEBUG = false,
-    FONTS = {
-        REDALERT = love.graphics.newFont('res/fonts/redalert.ttf', 13)
-    },
-}
