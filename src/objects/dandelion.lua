5bab0b1cc81b83d2723497be8d6cbd80e939ddb7
dvdfu <davidf1212@gmail.com>
2017-04-24 13:26:20 -0700

    hookup dandelion

--- src/objects/dandelion.lua
+++ src/objects/dandelion.lua
@@ -2,52 +2,42 @@ local Class = require 'modules.hump.class'
 local Object = require 'src.objects.object'
 local Constants = require 'src.constants'
 
-local sprites = {
-    petals = love.graphics.newImage('res/flower_petals.png'),
-    stamen = love.graphics.newImage('res/flower_stamen.png'),
-    stem = love.graphics.newImage('res/flower_stem.png'),
-    pollen = love.graphics.newImage('res/flower_pollen.png'),
-    splash = love.graphics.newImage('res/droplet.png'),
-    dropletSmall = love.graphics.newImage('res/droplet_small.png')
-}
-
 local Dandelion = Class.new()
 Dandelion:include(Object)
 
-local Class = require 'modules.hump.class'
-local Object = require 'src.objects.object'
-local Constants = require 'src.constants'
-
-
 local sprites = {
-    seeds = love.graphics.newImage('res/seeds.png'),
+    seeds = love.graphics.newImage('res/dandelion_puff.png'),
+    core = love.graphics.newImage('res/dandelion_core.png'),
 }
 
 local Seeds = Class.new()
 Seeds:include(Object)
 
-function Seeds:init(objects, x, y)
+function Seeds:init(objects, x, y, radius)
     Object.init(self, objects, x, y)
-    self:build(objects:getWorld(), x, y)
+    self:build(objects:getWorld(), x, y, radius)
+    self.radius = radius
     self:addTag('seeds')
 end
 
-function Seeds:build(world, x, y)
+function Seeds:build(world, x, y, radius)
     self.body = love.physics.newBody(world, x, y, 'dynamic')
     self.body:setSleepingAllowed(false)
-    self.shape = love.physics.newCircleShape(40)
+    self.shape = love.physics.newCircleShape(radius)
     self.fixture = love.physics.newFixture(self.body, self.shape)
+    self.fixture:setDensity(0.1)
     self.fixture:setUserData(self)
 end
 
 function Seeds:draw()
     local x, y = self.body:getPosition()
-    love.graphics.draw(sprites.seeds, x, y, self.body:getAngle(), 1, 1, 32, 32)
+    love.graphics.draw(sprites.core, x, y, 0, 1, 1, 16, 16)
+    love.graphics.draw(sprites.seeds, x, y, 0, self.radius / 36, self.radius / 36, 40, 40)
 end
 
 function Seeds:update(dt)
     Object.update(self, dt)
-    self.body:applyForce(0, -0.5)
+    self.body:applyForce(0, -1)
 end
 
 --------------------------------------------------------------------------------
@@ -60,9 +50,9 @@ function Dandelion:init(objects, x, y)
     self:build(objects:getWorld(), x, Constants.GAME_HEIGHT)
     self:addTag('dandelion')
     self.height = Constants.GAME_HEIGHT - y
-    self.seeds = Seeds(objects, x, y)
+    self.seeds = Seeds(objects, x, y, math.random(30, 40))
     local a, b = self.seeds.body:getPosition()
-    self.rope = love.physics.newRopeJoint(self.body, self.seeds.body, x, Constants.GAME_HEIGHT, a, b + 30, self.height, true)
+    self.rope = love.physics.newRopeJoint(self.body, self.seeds.body, x, Constants.GAME_HEIGHT, a, b, self.height, true)
 end
 
 function Dandelion:build(world, x, y)
@@ -78,9 +68,9 @@ function Dandelion:draw()
 
     love.graphics.push('all')
         local x1, y1, x2, y2 = self.rope:getAnchors()
-        love.graphics.setLineWidth(3)
-        love.graphics.setColor(178, 219, 151)
-        love.graphics.line(x1, y1, x2, y2-2)
+        love.graphics.setLineWidth(8)
+        love.graphics.setColor(135, 156, 107)
+        love.graphics.line(x1, y1, x2, y2 - 2)
     love.graphics.pop()
 end
 
